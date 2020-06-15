7c3d70a0f68946b0e396bcaeff7123c504a4f3ab
Johannes Doll <doll.johannes.94@gmail.com>
2020-06-16 01:06:24 +0200

    Add a ViewPager to MainActivity and show multiple RemoteControlPages

--- app/src/main/java/com/johannesdoll/avbus/MainActivity.kt
+++ app/src/main/java/com/johannesdoll/avbus/MainActivity.kt
@@ -2,6 +2,8 @@ package com.johannesdoll.avbus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.johannesdoll.avbus.ui.remotecontrol.view.RemoteControlFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -10,11 +12,20 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(toolbar)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, RemoteControlFragment.newInstance())
-                .commitNow()
+        device_view_pager.apply {
+            adapter = RemoteControlDeviceAdapter()
         }
+    }
+
+    private inner class RemoteControlDeviceAdapter : FragmentStateAdapter(this) {
+        private val pages = listOf<(() -> Fragment)>(
+            { RemoteControlFragment.newInstance() },
+            { RemoteControlFragment.newInstance() }
+        )
+
+        override fun getItemCount() = pages.size
+
+        override fun createFragment(position: Int) = pages[position].invoke()
     }
 
 }
